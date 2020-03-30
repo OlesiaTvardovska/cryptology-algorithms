b76aa9726c367c4cffccdc8d547b1a77a1dd6665
Olesia Tvardovska <tvardovska.29@gmail.com>
2020-03-30 10:15:24 +0300

    Add implementation of Cardan cipher

--- Cryptology/CardanoCipher.cs
+++ Cryptology/CardanoCipher.cs
@@ -9,9 +9,124 @@ namespace Cryptology
 {
     public class CardanoCipher : Cipher
     {
+        private const int size = 4;
+
         public CardanoCipher(string text) : base(text)
         {
+        }
 
+        public int[,] RotateMatrix(int[,] mtrx)
+        {
+            var dim = mtrx.GetLength(0);
+            var result = new int[dim, dim];
+
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    result[i, j] = mtrx[dim-1-j, i];
+                }
+            }
+            return result;
+        }
+        
+
+        public string[] GetStrArrayFromText(string text)
+        {
+            int length = text.Length / (size * size);
+            var result = new string[length];
+            for(int i = 0;i< length; i++)
+            {
+                result[i] = (text.Substring(i * size*size, size*size));
+            }
+            return result;
+        }
+
+        public override string Encrypt()
+        {
+            int[,] matrx = new int[size, size];
+            matrx = new int[size, size] {
+                {1, 0, 0, 0} ,
+                {0, 0, 1, 0} ,
+                {0, 0, 0, 1} ,
+                {0, 0, 1, 0}
+            };
+            string textExample = Text2Encode;
+
+            int sizeCalc = size*size;
+
+            var result = new StringBuilder();
+            char[] charArr = textExample.ToCharArray();
+            int value = 0;
+            int[] matrixArr = new int[sizeCalc];
+            for(int rotation = 0; rotation < size; rotation++)
+            {
+                matrx = RotateMatrix(matrx);
+                for(int i = 0; i < size; i++)
+                {
+                    for(int j = 0; j < size ; j++)
+                    {
+                        if(matrx[i, j] == 1)
+                        {
+                            matrixArr[i*size+j] = matrixArr.Max() + 1;
+                        }
+                    }
+                }
+            }
+            var textArray = GetStrArrayFromText(textExample);
+            foreach(var text in textArray)
+            {
+                string tmpText = "";
+                for(int i = 0; i < text.Length; i++)
+                {
+                    tmpText = tmpText + text[matrixArr[i] - 1];
+                }
+                result.Append(tmpText);
+            }
+            EncodedText = result.ToString();
+            return EncodedText;
+
+        }
+
+        public override string Decrypt()
+        {
+
+            var result = new StringBuilder(size*size);
+
+            int sizeCalc = size * size;
+            var textArray = GetStrArrayFromText(EncodedText);
+
+            var matrx = new int[size, size] {
+                {1, 0, 0, 0} ,
+                {0, 0, 1, 0} ,
+                {0, 0, 0, 1} ,
+                {0, 0, 1, 0}
+            };
+            int[] matrixArr = new int[sizeCalc];
+            for (int rotation = 0; rotation < size; rotation++)
+            {
+                matrx = RotateMatrix(matrx);
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (matrx[i, j] == 1)
+                        {
+                            matrixArr[i * size + j] = matrixArr.Max() + 1;
+                        }
+                    }
+                }
+            }
+            foreach (var text in textArray)
+            {
+                var tempResult = new char[size*size];
+                for (int i = 0;i<sizeCalc; i++)
+                {
+                    tempResult[matrixArr[i] - 1] = text[i];
+                }
+                DecryptedText = DecryptedText + new string(tempResult);
+            }
+            return DecryptedText;
         }
     }
 }
--- Cryptology/Program.cs
+++ Cryptology/Program.cs
@@ -11,28 +11,35 @@ namespace Cryptology
     {
         static void Main(string[] args)
         {
-
+            var path = @"D:\Cryptology\Cryptology\Texts\";
             var startString =
                 "A Cardan grille is made from a sheet of fairly rigid paper or parchment, or from thin metal. The paper is ruled to represent lines of handwriting and rectangular areas are cut out at arbitrary intervals between these lines.\r\n\r\nAn encipherer places the grille on a sheet of paper and writes his message in the rectangular apertures, some of which might allow a single letter, a syllable, or a whole word. Then, removing the grille, the fragments are filled out to create a note or letter that disguises the true message. Cardano suggested drafting the text three times in order to smooth any irregularities that might indicate the hidden words.";
-            string text = File.ReadAllText(@"C:\Users\user\source\repos\Cryptology\Cryptology\Texts\text.txt");
-            //var cypher = new CaeserCipher(text, 1);
-            var cypher = new AffineCipher(text, 3, 4);
+            //string text = File.ReadAllText(path+"text.txt");
+           // var cypher = new CaeserCipher(text, 1);
+            //var cypher = new AffineCipher(text, 3, 4);
+
+
+            //for cardano cypher
+            string text = File.ReadAllText(path + "textExample.txt");
+            var cypher = new CardanoCipher(text);
+
 
             Console.WriteLine("Encoded text:");
             var encryptedText = cypher.Encrypt();
             Console.WriteLine(encryptedText);
-            File.WriteAllText(@"C:\Users\user\source\repos\Cryptology\Cryptology\Texts\textEncrypted.txt",encryptedText);
+            File.WriteAllText(path+ "textEncrypted.txt",encryptedText);
 
 
             Console.WriteLine("\nDecrypted text:");
             var decryptedText = cypher.Decrypt();
             Console.WriteLine(decryptedText);
-            File.WriteAllText(@"C:\Users\user\source\repos\Cryptology\Cryptology\Texts\textDecrypted.txt", decryptedText);
+            File.WriteAllText(path+"textDecrypted.txt", decryptedText);
 
             Console.WriteLine("\nDecoded text:");
             var decodedText = cypher.Decode();
             Console.WriteLine(decodedText);
-            File.WriteAllText(@"C:\Users\user\source\repos\Cryptology\Cryptology\Texts\textDecoded.txt", decodedText);
+            File.WriteAllText(path+"textDecoded.txt", decodedText);
+
             Console.ReadKey();
         }
     }
